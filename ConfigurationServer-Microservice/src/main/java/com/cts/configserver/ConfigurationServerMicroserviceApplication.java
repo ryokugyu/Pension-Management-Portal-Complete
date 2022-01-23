4d39440518520bfd28125a009174fcaa45fcee2c
Himanshu Nailwal <himanshunailwal.rhce@gmail.com>
2022-01-24 02:45:27 +0530

    github link added for configurations

--- ConfigurationServer-Microservice/src/main/java/com/cts/configserver/ConfigurationServerMicroserviceApplication.java
+++ ConfigurationServer-Microservice/src/main/java/com/cts/configserver/ConfigurationServerMicroserviceApplication.java
@@ -3,9 +3,11 @@ package com.cts.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigurationServerMicroserviceApplication {
 
 	public static void main(String[] args) {
